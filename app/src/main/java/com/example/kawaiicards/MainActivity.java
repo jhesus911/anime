d1b773b79ab9debd25bf55a7d71b47e9442d3a1b
unknown <Aprendiz@POPDFTCCSFSD022.FormacionSena.RED>
2022-10-27 16:47:46 -0500

    primer commit

---
+++ app/src/main/java/com/example/kawaiicards/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.kawaiicards;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        List items = new ArrayList();
+        items.add(new Anime(R.drawable.angel, "uchiha", 230));
+        items.add(new Anime(R.drawable.death, "Death Note", 456));
+        items.add(new Anime(R.drawable.fate, "obito", 3));
+        items.add(new Anime(R.drawable.nhk, "Paint",75656));
+        items.add(new Anime(R.drawable.suzumiya, "Nagato",7887));
+        items.add(new Anime(R.drawable.gara, "gara",7887));
+// Obtener el Recycler
+        recycler = (RecyclerView) findViewById(R.id.reciclador2);
+        recycler.setHasFixedSize(true);
+// Usar un administrador para
+         lManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(lManager);
+// Crear un nuevo adaptador
+        adapter = new AnimeAdapter(items);
+        recycler.setAdapter(adapter);
+    }
+}
